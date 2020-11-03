e7995a2104bf7b752cb187050f32e1247b4fa968
Wojciech Wolak <wolak.wojciech.fe@gmail.com>
2020-11-03 09:28:32 +0100

    aded people section

--- src/features/PopularPeople/index.js
+++ src/features/PopularPeople/index.js
@@ -1,22 +1,61 @@
-import React from 'react';
-import { Tile } from '../../common/Tile';
+import React, { useEffect, useState } from 'react';
+import Pagination from '../../common/Pagination';
+import { Image, SectionHeader, Title } from './styled';
 import { Wrapper } from '../../common/Wrapper';
-import { SectionHeader } from './styled';
+import { Tile } from '../../common/Tile';
+import axios from 'axios';
 
 const PopularPeople = () => {
-    return(
+    const [people, setPeople] = useState({
+        state: "loading",
+        people: [],
+    });
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const API_KEY = "9fa8df50ae92cd1be9d2b9ae814b33c6";
+    const IMAGE_LINK = "https://image.tmdb.org/t/p/w500";
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await axios.get(`https://api.themoviedb.org/3/person/popular?api_key=${API_KEY}&language=en-US&page=${currentPage}`)
+                setPeople({
+                    state: "success",
+                    people: response.data,
+                });
+            } catch (error) {
+                console.log(error);
+            }
+        })();
+
+    }, [currentPage]);
+
+    console.log(people);
+
+    return (
         <Wrapper>
             <SectionHeader>Popular People</SectionHeader>
-            <Wrapper itemList>
-                <Tile image />
-                <Tile image />
-                <Tile image />
-                <Tile image />
-                <Tile image />
-                <Tile image />
-                <Tile image />
-                <Tile image />
+            <Wrapper personsList>
+                {people.state === "success" ?
+                    people.people.results.map((item) =>
+                        <Tile
+                            key={item.id}
+                        >
+                            <Image src={`${IMAGE_LINK}${item.profile_path}`} alt="" />
+                            <Title>{item.name}</Title>
+
+
+                        </Tile>
+                    ) :
+                    <p>Ładowanie danych</p>
+                }
+
+
+
             </Wrapper>
+            <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} page={people.people.page} totalPages={people.people.total_pages}>
+
+            </Pagination>
         </Wrapper>
     );
 }
--- src/features/PopularPeople/styled.js
+++ src/features/PopularPeople/styled.js
@@ -6,3 +6,17 @@ export const SectionHeader = styled.h2`
     line-height: 1.2;
     color: #18181B;
 `;
+
+export const Image = styled.img`
+    max-width: 100%;
+`;
+
+export const Title = styled.h3`
+    font-weight: 500;
+    font-size: 22px;
+    line-height: 1.3;
+    color: #18181B;
+    margin-top: 16px;
+    margin-bottom:0;
+    text-align: center;
+`;
